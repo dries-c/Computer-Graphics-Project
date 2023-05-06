78ba6536a9fcc4fd85c36f443d95a5dc73a882a5
Seppe Nijsen <seppe.nijsen@student.uhasselt.be>
2023-05-06 16:29:51 +0200

    Stone break sound

--- src/utils/Interactable.h
+++ src/utils/Interactable.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include "AxisAlignedBB.h"
+#include "../sound/Sound.h"
+
 
 class Interactable {
 private:
@@ -11,7 +13,11 @@ public:
     virtual void onAttack() = 0;
     virtual void onInteract() = 0;
     [[nodiscard]] bool isAlive() const { return alive; }
-    void kill() { alive = false; }
+    void kill() {
+        alive = false;
+        Sound sound = Sound("stone4.ogg");
+        sound.play();
+    }
     [[nodiscard]] virtual std::vector<AxisAlignedBB> getBoundingBoxes() const = 0;
     [[nodiscard]] virtual glm::mat4 getModelMatrix() const = 0;
 };
