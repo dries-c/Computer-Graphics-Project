c6ce1662d986519c4d27f11a474456d536ac6066
Seppe Nijsen <seppe.nijsen@student.uhasselt.be>
2023-05-07 12:53:35 +0200

    added old FILEMAZEPARSER

---
+++ src/parser/FileMazeParser.cpp
@@ -0,0 +1,35 @@
+#include "FileMazeParser.h"
+#include "../utils/FileUtils.h"
+#include "PositionEnum.h"
+#include <iostream>
+
+FileMazeParser::FileMazeParser(const char *path) {
+    std::string mazeBuffer = FileUtils::getFileContents(path);
+
+    std::vector<PositionEnum> row;
+    for (char c: mazeBuffer) {
+        switch (c) {
+            case '#':
+                row.push_back(PositionEnum::WALL);
+                break;
+            case 'O':
+                row.push_back(PositionEnum::OBSTACLE);
+                break;
+            case ' ':
+                row.push_back(PositionEnum::EMPTY);
+                break;
+            case 'L':
+                row.push_back(PositionEnum::LIGHT);
+                break;
+            case '\n':
+                maze.push_back(row);
+                row.clear();
+                break;
+            default:
+                std::cout << "Unknown character: " << c << std::endl;
+        }
+    }
+
+    maze.push_back(row);
+    std::cout << "Maze loaded" << std::endl;
+}
